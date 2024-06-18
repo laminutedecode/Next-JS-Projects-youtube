7bfcf81ce238198b8742f9785064f44f4040c478
Jbtzcodes <laminutedecode@gmail.com>
2024-06-18 09:57:37 +0200

    UPDATE project 14 - services functions

--- 14-clerk-prisma/prisma/migrations/20240618072506_initial/migration.sql
+++ 14-clerk-prisma/prisma/migrations/20240618072506_initial/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "image" TEXT,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
     "clerkUserId" TEXT
